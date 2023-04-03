bd8bd79aaa3dd555ebb766b5aa045ba2ebe38306
Maor <maorsa9@gmail.com>
2023-04-03 12:19:04 +0300

    BSPMC2312-20,BSPMC2312-24,BSPMC2312-47: task 20,24,47 done

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header>
+      <Navbar
+        className="navbar navbar-expand-lg navbar-dark bg-primary"
+        collapseOnSelect
+      >
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img
+                alt=""
+                src="/SCE.png"
+                width="60"
+                height="60"
+                className="d-inline-block align-top"
+              />
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/conntact">
+                <Nav.Link>
+                  צור קשר <i className="fas fa-phone"></i>
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  התחברות <i className="fas fa-user"></i>
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
