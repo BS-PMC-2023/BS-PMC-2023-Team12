f8929b55ba59bdc8d2a0d40e972068347a47824f
Maor <maorsa9@gmail.com>
2023-04-05 11:39:53 +0300

    BSPMC2312-48: Adding routes for saving user in DB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cors());
+app.use('/api/users', userRoutes);
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
+});
 
 const PORT = process.env.PORT || 5000;
 
