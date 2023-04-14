c2520b6f12dadee2b749c0c35982c9075c9d66ea
Barsheshett <73143641+Barsheshett@users.noreply.github.com>
2023-04-14 18:42:45 +0300

    init all cards

--- frontend/src/components/ProductScreen/CardProd.js
+++ frontend/src/components/ProductScreen/CardProd.js
@@ -2,16 +2,14 @@ import React from 'react';
 
 const CardProd = (props) => {
   return (
-    <div className='card_prod'>
-        <div class="card" >
-            <img src="/SCE.png" class="card-img-top" alt="pic"/>
+        <div class="card_prod" >
+            <img src={props.img} class="card-img-top" alt="pic"/>
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">{props.header}</h5>
+                <p class="card-text">{props.p}</p>
+                <a href="#" class="btn btn-primary">{props.a}</a>
             </div>
         </div>
-    </div>
     
   );
 };
--- frontend/src/screens/ProductsScreen.js
+++ frontend/src/screens/ProductsScreen.js
@@ -3,20 +3,65 @@ import CardProd from '../components/ProductScreen/CardProd';
 
 const ProductsScreen = () => {
   return(
-    <div class="row row-cols-3 row-cols-2 ">
+      <div class="row row-cols-3 g-4 ">
         <div class="col">
-            <CardProd></CardProd>
+            <CardProd 
+            //img="/SCE.png" 
+            img="camera.jpg"
+            header="Camera" 
+            p="ציוד צילום" 
+            a="צפה בזמינות"/>
         </div>
         <div class="col">
-            <CardProd></CardProd>
+            <CardProd
+            img="/microphone.jpg" 
+            header="Recording" 
+            p="ציוד הקלטה" 
+            a="צפה בזמינות"/>
         </div>
         <div class="col">
-            <CardProd></CardProd>
+            <CardProd
+            img="/ipad.jpg" 
+            header="Apple" 
+            p="טאבלטים" 
+            a="צפה בזמינות"/>
         </div>
         <div class="col">
-            <CardProd></CardProd>
+            <CardProd
+            img="/tripod.jpg" 
+            header="Tripod" 
+            p="חצובות" 
+            a="צפה בזמינות"/>
         </div>
-    </div>
+        <div class="col">
+            <CardProd
+            img="/projector.jpg" 
+            header="Projectors" 
+            p="מקרנים" 
+            a="צפה בזמינות"/>
+        </div>
+        <div class="col">
+            <CardProd
+            img="/cables.jpg" 
+            header="Cables" 
+            p="כבלים" 
+            a="צפה בזמינות"/>
+        </div>
+        <div class="col">
+            <CardProd
+            img="/lights.jpg" 
+            header="Lights" 
+            p="תאורה" 
+            a="צפה בזמינות"/>
+        </div>
+        <div class="col">
+            <CardProd
+            img="/convertors.jpg" 
+            header="Convertos" 
+            p="ממירים" 
+            a="צפה בזמינות"/>
+        </div>
+      </div>
   );
    
 };
