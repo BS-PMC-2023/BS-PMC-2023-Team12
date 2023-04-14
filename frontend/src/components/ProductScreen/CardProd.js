f5528d090429b3b2b5bd0b7c8f9a81fc1ab2e26c
Barsheshett <73143641+Barsheshett@users.noreply.github.com>
2023-04-14 18:53:48 +0300

    add hover to cards css

--- frontend/src/components/ProductScreen/CardProd.js
+++ frontend/src/components/ProductScreen/CardProd.js
@@ -5,7 +5,7 @@ const CardProd = (props) => {
         <div class="card_prod" >
             <img src={props.img} class="card-img-top" alt="pic"/>
             <div class="card-body">
-                <h5 class="card-title">{props.header}</h5>
+                <h5 class="card-title-card-prod">{props.header}</h5>
                 <p class="card-text">{props.p}</p>
                 <a href="#" class="btn btn-primary">{props.a}</a>
             </div>
