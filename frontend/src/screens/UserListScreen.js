d83df222746958f887f365d43051f69f7dd7a4d8
Maor <maorsa9@gmail.com>
2023-05-08 17:03:28 +0300

    added confirm to delete user

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { LinkContainer } from 'react-router-bootstrap'
-import { Table, Button } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap';
+import { Table, Button } from 'react-bootstrap';
 import { useHttpClient } from '../hooks/httpHook';
 
-import Loader from '../components/Loader'
-
-
+import Loader from '../components/Loader';
 
 const UserListScreen = () => {
-  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const { isLoading, sendRequest } = useHttpClient();
   const [loadedUsers, setLoadedUsers] = useState();
 
   useEffect(() => {
@@ -17,20 +15,22 @@ const UserListScreen = () => {
         const responseData = await sendRequest(
           'http://localhost:5000/api/users'
         );
-
         setLoadedUsers(responseData.users);
       } catch (err) {}
     };
     fetchUsers();
   }, [sendRequest]);
-  
+
   const delete_user = async (user) => {
     try {
-      await sendRequest(
-        `http://localhost:5000/api/users/${user.email}`,
-        'DELETE'
-      );
+      var answer = window.confirm('בטוח שברצונך למחוק את המשתמש?');
+      if (answer) {
+        await sendRequest(
+          `http://localhost:5000/api/users/${user.email}`,
+          'DELETE'
+        );
         window.location.reload();
+      }
     } catch (err) {
       console.log(err);
     }
@@ -40,55 +40,53 @@ const UserListScreen = () => {
       <h1>רשימת משתמשים</h1>
       {isLoading ? (
         <Loader />
-      ) : 
-        <Table striped bordered hover responsive className='table-sm'>
+      ) : (
+        <Table striped bordered hover responsive className="table-sm">
           <thead>
             <tr>
-              <th>ID</th>
-              <th>NAME</th>
-              <th>EMAIL</th>
-              <th>ADMIN</th>
+              {/* <th>ת"ז</th> */}
+              <th>שם</th>
+              <th>אימייל</th>
+              <th>הרשאות מנהל</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
             {loadedUsers?.map((user) => (
               <tr key={user._id}>
-                <td>{user._id}</td>
+                {/* <td>{user._id}</td> */}
                 <td>{user.name}</td>
                 <td>
                   <a href={`mailto:${user.email}`}>{user.email}</a>
                 </td>
                 <td>
                   {user.isAdmin ? (
-                    <i className='fas fa-check' style={{ color: 'green' }}></i>
+                    <i className="fas fa-check" style={{ color: 'green' }}></i>
                   ) : (
-                    <i className='fas fa-times' style={{ color: 'red' }}></i>
+                    <i className="fas fa-times" style={{ color: 'red' }}></i>
                   )}
                 </td>
                 <td>
                   <LinkContainer to={`/admin/user/${user._id}/edit`}>
-                    <Button variant='light' className='btn-sm'>
-                      <i className='fas fa-edit'></i>
+                    <Button variant="light" className="btn-sm">
+                      <i className="fas fa-edit"></i>
                     </Button>
                   </LinkContainer>
                   <Button
-                    variant='danger'
-                    className='btn-sm'
-                    onClick={()=>delete_user(user)}
+                    variant="danger"
+                    className="btn-sm"
+                    onClick={() => delete_user(user)}
                   >
-                    <i className='fas fa-trash'></i>
+                    <i className="fas fa-trash"></i>
                   </Button>
                 </td>
               </tr>
             ))}
           </tbody>
         </Table>
-      }
+      )}
     </>
-  )
-}
-
-
+  );
+};
 
 export default UserListScreen;
