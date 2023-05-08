cefc67e3bd60f56090b4fc083e99ee758236d08a
Maor <maorsa9@gmail.com>
2023-05-08 17:05:40 +0300

    BSPMC2312-71,BSPMC2312-72,BSPMC2312-73,BSPMC2312-70; finshed with reset password fonctionalty

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const HttpError = require('../httpError');
 const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+var nodemailer = require('nodemailer');
 
 const getUsers = async (req, res, next) => {
   let users;
@@ -135,6 +136,103 @@ const login = async (req, res, next) => {
   });
 };
 
+const forogotPassword = async (req, res, next) => {
+  const { email } = req.body;
+  try {
+    const oldUser = await User.findOne({ email });
+
+    if (!oldUser) {
+      const error = new HttpError('משתמש לא קיים, אנא נסה שנית', 401);
+      return next(error);
+    }
+
+    const secret = process.env.JWT_SECRET + oldUser.password;
+    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
+      expiresIn: '10m',
+    });
+
+    const link = `http://localhost:5000/api/users/reset-password/${oldUser._id}/${token}`;
+
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'wmsvcteam12@gmail.com',
+        pass: 'kfgyejkhdbbvzmfm',
+      },
+    });
+
+    var mailOptions = {
+      from: 'wmsvcteam12@gmail.com',
+      to: email,
+      subject: 'SCEWMS ResetPassword',
+      text: `:לאיפוס הסיסמה כנס ללינק \n${link} \n\n .הלינק יהיה תקף למשך 10 דקות`,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+    //console.log(link);
+    res.json({
+      email: email,
+    });
+  } catch (err) {
+    const error = new HttpError(' נסה שוב.', 500);
+    return next(error);
+  }
+};
+
+const resetPassword = async (req, res) => {
+  const { id, token } = req.params;
+  const oldUser = await User.findOne({ _id: id });
+
+  if (!oldUser) {
+    return res.status(401).json({ message: 'User Does Not Exists' });
+  }
+
+  const secret = process.env.JWT_SECRET + oldUser.password;
+  try {
+    const verify = jwt.verify(token, secret);
+    res.render('index', { email: verify.email, status: 'Not Verified' });
+  } catch (error) {
+    console.log(error);
+    res.send('Not Verified');
+  }
+};
+
+const changePassword = async (req, res) => {
+  const { id, token } = req.params;
+  const { password } = req.body;
+
+  const oldUser = await User.findOne({ _id: id });
+
+  if (!oldUser) {
+    return res.status(401).json({ message: 'User Does Not Exists' });
+  }
+
+  const secret = process.env.JWT_SECRET + oldUser.password;
+  try {
+    const verify = jwt.verify(token, secret);
+    const encryptedPassword = await bcrypt.hash(password, 12);
+    await User.updateOne(
+      {
+        _id: id,
+      },
+      {
+        $set: {
+          password: encryptedPassword,
+        },
+      }
+    );
+    res.render('index', { email: verify.email, status: 'verified' });
+  } catch (error) {
+    res.json({ status: 'Something Went Wrong' });
+  }
+};
+
 const deleteUser = async (req, res, next) => {
   const userId = req.params.id;
   console.log(req.params.id);
@@ -172,3 +270,6 @@ exports.getUsers = getUsers;
 exports.deleteUser = deleteUser;
 exports.register = register;
 exports.login = login;
+exports.forogotPassword = forogotPassword;
+exports.resetPassword = resetPassword;
+exports.changePassword = changePassword;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,17 +1,15 @@
-const User = require('../models/userModel');
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const usersController = require('../controllers/userController');
 
 const router = express.Router();
 
-const app = express();
-
-
 router.get('/', usersController.getUsers);
-router.delete('/:id',usersController.deleteUser);
+router.delete('/:id', usersController.deleteUser);
 router.post('/register', usersController.register);
 router.post('/login', usersController.login);
+router.post('/forgot-password', usersController.forogotPassword);
+router.get('/reset-password/:id/:token', usersController.resetPassword);
+router.post('/reset-password/:id/:token', usersController.changePassword);
 
 module.exports = router;
