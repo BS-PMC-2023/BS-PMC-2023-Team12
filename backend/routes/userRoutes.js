1c1a1860bbff4cf08700b46659d956d49157f211
AmirFukman1 <128290659+AmirFukman1@users.noreply.github.com>
2023-05-11 11:02:45 +0300

    bug fixes in admin rights

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,6 +12,6 @@ router.post('/forgot-password', usersController.forogotPassword);
 router.get('/reset-password/:id/:token', usersController.resetPassword);
 router.post('/reset-password/:id/:token', usersController.changePassword);
 router.get('/personalZone', usersController.updateUserProfile).put(usersController.updateUserProfile);
-router.put('/updateAdmin/:id/:token', usersController.updateAdmin);
+router.put('/updateAdmin/:id', usersController.updateAdmin);
 
 module.exports = router;
