a38b0ec86a578adf2935d7fb845dba376183256e
Maor <maorsa9@gmail.com>
2023-05-21 09:51:20 +0300

    BSPMC2312-103,BSPMC2312-104,BSPMC2312-105,BSPMC2312-78; finished with the notification system task.

--- backend/controllers/borrowConroller.js
+++ backend/controllers/borrowConroller.js
@@ -1,5 +1,90 @@
 const HttpError = require('../httpError');
 const Borrow = require('../models/borrowModel');
+var nodemailer = require('nodemailer');
+const cron = require('node-cron');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'wmsvcteam12@gmail.com',
+    pass: 'kfgyejkhdbbvzmfm',
+  },
+});
+
+function sendEmail() {
+  let currentDate = new Date();
+
+  const cureent = currentDate;
+  const fyyyy = cureent.getFullYear();
+  let fmm = cureent.getMonth() + 1;
+  let fdd = cureent.getDate();
+
+  if (fdd < 10) fdd = '0' + fdd;
+  if (fmm < 10) fmm = '0' + fmm;
+
+  const formattedcurrentDate = fdd + '/' + fmm + '/' + fyyyy;
+  currentDate = formattedcurrentDate;
+
+  Borrow.find({ returnDate: { $lte: currentDate }, isAvailable: false })
+    .then((borrows) => {
+      borrows.forEach((borrow) => {
+        if (currentDate >= borrow.returnDate) {
+          const borrowerMailOptions = {
+            from: 'wmsvcteam12@gmail.com',
+            to: borrow.email,
+            subject: 'תזכורת להחזרת ציוד',
+            text: generateBorrowerEmailText(borrow),
+          };
+
+          const managerMailOptions = {
+            from: 'wmsvcteam12@gmail.com',
+            to: 'admin@ac.sce.ac.il', // Replace with the manager's email address
+            subject: `תזכורת להחזרת ציוד - לקוח: ${borrow.name}, מק"ט: ${borrow.equipmentID}`,
+            text: generateManagerEmailText(borrow),
+          };
+
+          transporter.sendMail(borrowerMailOptions, (error, info) => {
+            if (error) {
+              console.error(error);
+            } else {
+              console.log(`Borrower Email sent: ${info.response}`);
+            }
+          });
+
+          transporter.sendMail(managerMailOptions, (error, info) => {
+            if (error) {
+              console.error(error);
+            } else {
+              console.log(`Manager Email sent: ${info.response}`);
+            }
+          });
+        }
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+function generateBorrowerEmailText(borrow) {
+  let emailText = `,שלום יקר/ה משתמש/ת\n\n`;
+  emailText += `.רצינו להזכירך כי עליך להחזיר את הציוד שהשאלתה מספר מק"ט: ${borrow.equipmentID}\n`;
+  emailText += `.תאריך החזרה נקבע ל-${borrow.returnDate}\n`;
+  emailText += `.אנא וודא כי מבצעים את ההחזרה בזמן כדי שנוכל לשרת גם את המשתמשים האחרים\n\n`;
+  emailText += `,תודה רבה ובכבוד רב`;
+  emailText += `\n\n SCEWMS צוות`;
+  return emailText;
+}
+
+function generateManagerEmailText(borrow) {
+  const subject = `תזכורת להחזרת ציוד - לקוח: ${borrow.name}, מק"ט: ${borrow.equipmentID}`;
+  return subject;
+}
+
+// Schedule sendEmail to run every day at 10 AM
+cron.schedule('0 10 * * *', () => {
+  sendEmail();
+});
 
 const addBorrow = async (req, res) => {
   const { userID, equipmentID, name, email, borrowDate, returnDate } = req.body;
