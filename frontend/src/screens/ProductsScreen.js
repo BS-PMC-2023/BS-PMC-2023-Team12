4a50b619be835dcbd792b7ae4e77e40e45a78746
yovel derey <yovelderey@gmail.com>
2023-05-27 12:13:08 +0300

    fix bug in master - productScreen

--- frontend/src/screens/ProductsScreen.js
+++ frontend/src/screens/ProductsScreen.js
@@ -1,73 +1,107 @@
 import React from 'react';
 import CardProd from '../components/ProductScreen/CardProd';
+import { Routes, Route, useParams } from 'react-router-dom';
+import { Form } from 'react-bootstrap';
+
+const products = [
+  {
+    
+    img: "camera.jpg",
+    header: "Camera",
+    p: "ציוד צילום",
+    href: "./CamerasScreen"
+  },
+  {
+    img: "/microphone.jpg",
+    header: "Recording",
+    p:"ציוד הקלטה" ,
+    a:"צפה בזמינות",
+    href: "./RecordingScreen"
+  },
+  {
+    img:"/ipad.jpg" ,
+    header:"Apple" ,
+    p:"טאבלטים" ,
+    a:"צפה בזמינות",
+    href: "./AppleScreen"
+  },
+  {
+    img:"/tripod.jpg" ,
+    header:"Tripod" ,
+    p:"חצובות" ,
+    a:"צפה בזמינות",
+    href: "./TripodScreen"
+  },
+  {
+    img:"/projector.jpg" ,
+    header:"Projectors" ,
+    p:"מקרנים" ,
+    a:"צפה בזמינות",
+    href: "./ProjectorsScreen"
+  },
+  {
+    img:"/cables.jpg" ,
+    header:"Cables" ,
+    p:"כבלים" ,
+    a:"צפה בזמינות",
+    href: "./CablesScreen"
+  },
+  {
+    img:"/lights.jpg" ,
+    header:"Lights" ,
+    p:"תאורה" ,
+    a:"צפה בזמינות",
+    href: "./LightsScreen"
+  },
+  {
+    img:"/convertors.jpg" ,
+    header:"Convertos" ,
+    p:"ממירים" ,
+    a:"צפה בזמינות",
+    href: "./ConvertorsScreen"
+  }
+];
 
 const ProductsScreen = () => {
-  return(
-      <div class="row row-cols-3 g-4 ">
-        <div class="col">
-            <CardProd 
-            //img="/SCE.png" 
-            img="camera.jpg"
-            header="Camera" 
-            p="ציוד צילום" 
-            a="צפה בזמינות"
-            href="./CamerasScreen"
-            />
-        </div>
-        <div class="col">
-            <CardProd
-            img="/microphone.jpg" 
-            header="Recording" 
-            p="ציוד הקלטה" 
-            a="צפה בזמינות"
-            href="./RecordingScreen"
-            />
-        </div>
-        <div class="col">
-            <CardProd
-            img="/ipad.jpg" 
-            header="Apple" 
-            p="טאבלטים" 
-            a="צפה בזמינות"/>
-        </div>
-        <div class="col">
-            <CardProd
-            img="/tripod.jpg" 
-            header="Tripod" 
-            p="חצובות" 
-            a="צפה בזמינות"/>
-        </div>
-        <div class="col">
-            <CardProd
-            img="/projector.jpg" 
-            header="Projectors" 
-            p="מקרנים" 
-            a="צפה בזמינות"/>
-        </div>
-        <div class="col">
-            <CardProd
-            img="/cables.jpg" 
-            header="Cables" 
-            p="כבלים" 
-            a="צפה בזמינות"/>
-        </div>
-        <div class="col">
-            <CardProd
-            img="/lights.jpg" 
-            header="Lights" 
-            p="תאורה" 
-            a="צפה בזמינות"/>
-        </div>
-        <div class="col">
-            <CardProd
-            img="/convertors.jpg" 
-            header="Convertos" 
-            p="ממירים" 
-            a="צפה בזמינות"/>
-        </div>
-      </div>
+  const [keyword, setKeyword] = React.useState('');
+  const [filteredProducts, setFilteredProducts] = React.useState([]);
+
+  React.useEffect(() => {
+    if (keyword === '') {
+      setFilteredProducts(products);
+    } else {
+      const filtered = products.filter((product) => product.p === keyword);
+      setFilteredProducts(filtered);
+    }
+  }, [keyword]);
+
+  const productCards = filteredProducts.map((product) => (
+    <div className="col" key={product.id}>
+      <CardProd
+        img={product.img}
+        header={product.header}
+        p={product.p}
+        a={product.a}
+        href={product.href}
+      />
+    </div>
+  ));
+
+  return (
+    <div>
+      <Form>
+        <Form.Group controlId="searchForm">
+          <Form.Control
+            type="text"
+            placeholder="Search"
+            value={keyword}
+            onChange={(e) => setKeyword(e.target.value)}
+          />
+        </Form.Group>
+      </Form>
+      <div className="row row-cols-3 g-4">{productCards}</div>
+    </div>
   );
-   
 };
 
 export default ProductsScreen;
