ba5c67ff9142ed4e006ee45c2b45f9fd641ff784
Yaronsa1 <95167633+Yaronsa1@users.noreply.github.com>
2023-05-31 18:19:27 +0300

    fixed Styling issues

--- frontend/src/screens/ContactPageScreen.js
+++ frontend/src/screens/ContactPageScreen.js
@@ -14,23 +14,49 @@ const ContactPageScreen = () => {
           For any inquiries or support, please reach out to us via email:
         </p>
         <ul>
-          <li>Email: <a href="mailto:RandomEmail@gmail.com">RandomEmail@gmail.com</a></li>
+          <li>
+            Email:{' '}
+            <a href="mailto:RandomEmail@gmail.com">RandomEmail@gmail.com</a>
+          </li>
         </ul>
       </div>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Your Name:</label>
-          <input type="text" id="name" name="name" />
+        <div style={{ marginBottom: '16px' }}>
+          <label htmlFor="name" style={{ display: 'block', marginBottom: '4px' }}>
+            Your Name:
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            style={{ padding: '8px', width: '100%' }}
+          />
         </div>
-        <div>
-          <label htmlFor="email">Your Email:</label>
-          <input type="email" id="email" name="email" />
+        <div style={{ marginBottom: '16px' }}>
+          <label htmlFor="email" style={{ display: 'block', marginBottom: '4px' }}>
+            Your Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            style={{ padding: '8px', width: '100%' }}
+          />
         </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea id="message" name="message" rows="4"></textarea>
+        <div style={{ marginBottom: '16px' }}>
+          <label htmlFor="message" style={{ display: 'block', marginBottom: '4px' }}>
+            Message:
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            rows="4"
+            style={{ padding: '8px', width: '100%' }}
+          ></textarea>
         </div>
-        <button type="submit">Submit</button>
+        <button type="submit" style={{ padding: '8px 16px' }}>
+          Submit
+        </button>
       </form>
     </div>
   );
